01c456878b07607b116ffc44bde66b769a690e36
Alina <alinbogz@gmail.com>
2022-09-15 14:01:15 +0200

    returning functions

--- functions.js
+++ functions.js
@@ -33,3 +33,34 @@ console.log(flight);
 console.log(jonas);
 
 console.log('----higherOrderFunctions--------');
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' '); //join() returns an array as a string
+};
+const transformer = function (str, fn) {
+  console.log(`Original string:${str}`);
+  console.log(`Transformed string:${fn(str)}`);
+  console.log(`Transfomred by: ${fn.name}`);
+};
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+console.log('---new ex---');
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet('Hey');
+greeterHey('Alina');
+greeterHey('Dinu');
+//OR THE SAME BUT ARROW FUNCTION
+
+const dreet = greeting => name => console.log(`${greeting} ${name}`);
+
+dreet('Hey')('Aliosha');
