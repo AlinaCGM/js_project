775041730e2a180c5c74d4b938d75a720bae05a7
Alina <alinbogz@gmail.com>
2022-10-13 09:19:42 +0200

    reveal section

--- bankist.js
+++ bankist.js
@@ -132,7 +132,6 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 //another way to do that
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 const stickyNav = function (entries) {
   const [entry] = entries; //or destructure other way const [entry]=entries[0]
   console.log(entry);
@@ -145,6 +144,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+//
+//
+//-----------------Reveal sections 8fade in effect
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  console.log(entry);
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 //////////////////////////////////
 //  ------------------------LESSON--------------------
